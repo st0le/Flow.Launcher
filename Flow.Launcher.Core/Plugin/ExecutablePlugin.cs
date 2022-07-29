ee1c8fedb314e658ed79bd3aa9347c5f1ef89d24
st0le <no-reply@example.com>
2022-07-28 20:07:50 -0700

    Add Arguments Config for Executable Plugin

--- Flow.Launcher.Core/Plugin/ExecutablePlugin.cs
+++ Flow.Launcher.Core/Plugin/ExecutablePlugin.cs
@@ -9,9 +9,10 @@ namespace Flow.Launcher.Core.Plugin
     internal class ExecutablePlugin : JsonRPCPlugin
     {
         private readonly ProcessStartInfo _startInfo;
+        private readonly int argIndex;
         public override string SupportedLanguage { get; set; } = AllowedLanguage.Executable;
 
-        public ExecutablePlugin(string filename)
+        public ExecutablePlugin(string filename, string arguments)
         {
             _startInfo = new ProcessStartInfo
             {
@@ -21,7 +22,19 @@ namespace Flow.Launcher.Core.Plugin
                 RedirectStandardOutput = true,
                 RedirectStandardError = true
             };
-            
+
+            if (string.IsNullOrEmpty(arguments))
+            {
+                // add arguments to Executable
+                // For eg. FileName = powershell.exe, Arguments = -NoProfile -File pluginscript.ps1
+                _startInfo.ArgumentList.Add(arguments);
+                argIndex = 1;
+            }
+            else
+            {
+                argIndex = 0;
+            }
+
             // required initialisation for below request calls 
             _startInfo.ArgumentList.Add(string.Empty);
         }
@@ -29,14 +42,14 @@ namespace Flow.Launcher.Core.Plugin
         protected override Task<Stream> RequestAsync(JsonRPCRequestModel request, CancellationToken token = default)
         {
             // since this is not static, request strings will build up in ArgumentList if index is not specified
-            _startInfo.ArgumentList[0] = request.ToString();
+            _startInfo.ArgumentList[argIndex] = request.ToString();
             return ExecuteAsync(_startInfo, token);
         }
 
         protected override string Request(JsonRPCRequestModel rpcRequest, CancellationToken token = default)
         {
             // since this is not static, request strings will build up in ArgumentList if index is not specified
-            _startInfo.ArgumentList[0] = rpcRequest.ToString();
+            _startInfo.ArgumentList[argIndex] = rpcRequest.ToString();
             return Execute(_startInfo);
         }
     }
--- Flow.Launcher.Plugin/PluginMetadata.cs
+++ Flow.Launcher.Plugin/PluginMetadata.cs
@@ -20,6 +20,8 @@ namespace Flow.Launcher.Plugin
 
         public string ExecuteFileName { get; set; }
 
+        public string ExecuteArguments { get; set; }
+
         public string PluginDirectory
         {
             get { return _pluginDirectory; }
